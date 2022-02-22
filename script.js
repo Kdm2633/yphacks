66ec746cd7d01580908ea8eff2397655e241a1c1
Kamya Miles <kmilesstem2023@gmail.com>
2022-02-21 23:59:55 -0500

    Creating layout and js files

---
+++ script.js
@@ -0,0 +1,5 @@
+//To Do:
+//Add function that compares search input with user database
+//Add function that displays friend list
+//Add function that adds and removes friend from list.
+//Add function that matches user to people with similar study topics selected
