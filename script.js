6993c718d9c984ceec22ec3e1df1498bd8903188
Kamya Miles <kmilesstem2023@gmail.com>
2022-02-23 16:53:16 -0500

    Js Updates

--- script.js
+++ script.js
@@ -7,31 +7,61 @@
 var users = ["bob", "kamya", "sean", "nihal", "sam"];
 var kamyaFriends = ["bob", "sam"];
 
+//When a user press enter after entering an input, the website will search for users
 var search = document.getElementById("searchInput");
 search.addEventListener("keypress", event =>{
   if(event.key === "Enter"){
-    document.querySelector("profileList").append('<div class="userProfile"><img src="assets/user.png" alt="username"></img> <p>name</p></div>');
+   userSearch();
+  }
+  if(event.key==="m"){ //For testing purposes for the clearSearchOutput method
+      clearSearchOutput();
   }
 });
 
+//Remove this after testing
 function testing(){
     console.log("123");
 }
 
 function userSearch(){
-  for (let i = 0; i < users.length; i++) {
-    if(users[i].includes(document.getElementById("searchInput"))){
-        console.log("working");
-        const profileList = document.querySelector(".profileList");
-        const newUserProfile = document.createElement("DIV");
-        const newUserImg = document.createElement("IMG");
-        const newUserName = document.createElement("P");
+    var numUsersFound = 0;
+    //Goes through users collection to see if a user has the searched username
+  for (var i = 0; i < users.length; i++) {
+    if(users[i].includes(search.value)){
+        addProfile();
+        numUsersFound++;
+     }
+  }
+  //If no users are found then it will display a no users found message.
+  //bug: when just pressing enter it returns it's searching for something, idk why 
+  if(numUsersFound==0){
+    const profileList = document.querySelector(".profileList");
+    const noUserImg = document.createElement("IMG");
+    noUserImg.setAttribute("src", "assets/mug-hot.png");
+    noUserImg.setAttribute("class", "noUserFound");
+    const label = document.createElement("H1");
+    label.innerText = "No Users Found";
 
-        profileList.appendChild(newUserProfile);
-        newUserProfile.appendChild(newUserImg, newUserName);
-     }else{
-        console.log("no name found");
-        document.getElementsByClassName("profileList").innerHTML += '<img class="noUserFound" alt="no users found" src="assets/mug-hot.png"><h1>No Users Found</h1>';
-    }
-  };
+    profileList.appendChild(noUserImg, label);
+ } 
 }
+
+function addProfile(){
+    const profileList = document.querySelector(".profileList");
+    const newUserProfile = document.createElement("DIV");
+    newUserProfile.setAttribute("class","userProfile");
+    const newUserImg = document.createElement("IMG");
+    newUserImg.setAttribute("src", "assets/user.png");
+    const newUserName = document.createElement("P");
+    newUserName.innerText = "name";
+
+    profileList.appendChild(newUserProfile);
+    newUserProfile.appendChild(newUserImg, newUserName);
+}
+
+function clearSearchOutput(){
+    const profileList = document.querySelector(".profileList");
+    profileList.removeChild(document.querySelector("userProfile"));   
+}
+
+
