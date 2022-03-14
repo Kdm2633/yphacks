b27b475dfa5bd7b495230a6b926a5e4345bb3293
Sean K <87321511+Seanrk06@users.noreply.github.com>
2022-03-13 23:27:18 -0400

    commit to 'main'

---
+++ buddy-page/data.js
@@ -0,0 +1,56 @@
+var users = [
+    {
+        id: 0,
+        username: "bob",
+        password: "bb",
+        pfp: "./assets/ginger-twins.png",
+        friendRequests: [],
+        friends: [1],
+        studyGroups: []
+    },
+    {
+        id: 1,
+        username: "kamyakdm",
+        password: "rosesAreRed",
+        pfp: "./assets/cute-pfp.png",
+        friendRequests: [],
+        friends: [0, 4],
+        studyGroups: ["AP Chem", "AP World"]
+    },
+    {
+        id: 2,
+        username: "seanNN",
+        password: "violetsAreBlue2",
+        pfp: "./assets/user.png",
+        friendRequests: [],
+        friends: [],
+        studyGroups: []
+    },
+    {
+        id: 3,
+        username: "niHal",
+        password: "pythonSlitherR",
+        pfp: "./assets/user.png",
+        friendRequests: [],
+        friends: [],
+        studyGroups: []
+    },
+    {
+        id: 4,
+        username: "sam",
+        password: "eggs&greenham",
+        pfp: "./assets/cat.png",
+        friendRequests: [],
+        friends: [1],
+        studyGroups: []
+    },
+    {
+        id: 5,
+        username: "asdfghjklgd",
+        password: "K3y$m@$hR0cKs",
+        pfp: "./assets/user.png",
+        friendRequests: [],
+        friends: [],
+        studyGroups: [] 
+    }
+]
---
+++ buddy-page/script.js
@@ -0,0 +1,122 @@
+//To Do:
+//Add function that adds and removes friend from list.
+//Add function that matches user to people with similar study topics selected
+
+//Globals
+var search = document.getElementById("searchInput");
+const userPFP = document.getElementById("profilePic");
+var userID;
+
+//user currently logged in for testing purposes
+loadUserInfo("sam");
+
+//Events
+search.addEventListener("keypress", event =>{
+  if(event.key === "Enter"){
+    clearSearchOutput();   
+    userSearch();
+  }
+});
+
+userPFP.addEventListener("mouseenter", event => {
+console.log("hovered");
+});
+
+//Functions
+
+//Remove this after testing
+function testing(){
+    console.log("123");
+}
+
+  function userSearch(){
+    var numUsersFound = 0;
+    //Goes through users collection to see if a user has the searched username
+  for (var i = 0; i < users.length; i++) {
+    if(users[i].username.includes(search.value)){
+        addProfile("searchedList", users[i].username, users[i].pfp);
+        numUsersFound++;
+     }
+  }
+  //If no users are found then it will display a no users found message.
+  //bug: when just pressing enter it returns it's searching for something, idk why 
+  if(numUsersFound==0){
+    const profileList = document.getElementById("searchedList");
+    const noUserContainer = document.createElement("DIV");
+    noUserContainer.setAttribute("class", "noUserFound");
+    const noUserImg = document.createElement("IMG");
+    noUserImg.setAttribute("src", "assets/mug-hot.png");
+    const noUserText = document.createElement("H1");
+    noUserText.id = "noUserFoundTxt";
+    profileList.appendChild(noUserContainer);
+    noUserContainer.appendChild(noUserImg);
+    noUserContainer.appendChild(noUserText);
+    document.getElementById(noUserText.id).innerHTML = "No Users Found";
+ } 
+}
+
+//Adds profiles found to outputs
+function addProfile(target, user, picture){
+    const profileList = document.getElementById(target);
+    const newUserProfile = document.createElement("DIV");
+    newUserProfile.setAttribute("class","userProfile");
+    const newUserImg = document.createElement("IMG");
+    newUserImg.setAttribute("src", picture);
+    newUserImg.setAttribute("class", "userProfileImg");
+    const newUserName = document.createElement("p");
+    newUserName.id= user + "user";
+
+    //Adding elements to user profile container
+    profileList.appendChild(newUserProfile);
+    newUserProfile.appendChild(newUserImg);
+    newUserProfile.appendChild(newUserName);
+    if (target === "searchedList"){
+      const addBtn = document.createElement("IMG");
+      addBtn.setAttribute("src", "assets/add-icon.png");
+      addBtn.setAttribute("class", "addButton");
+      var userBtn = user + "AddButton";
+      addBtn.id= userBtn;
+      newUserProfile.appendChild(addBtn);
+      document.getElementById(addBtn.id).setAttribute("onclick", "addFriend("+user+")");
+    }
+    document.getElementById(newUserName.id).innerHTML = user;
+}
+
+//clears search output everytime a user is searched
+function clearSearchOutput(){
+    const profileList = document.getElementById("searchedList");
+    while(profileList.childElementCount>0){
+      profileList.lastChild.remove();
+    }
+}
+
+function addFriend(user){
+  console.log("hello " + user);
+}
+
+function removeFriend(){
+  
+}
+
+//Loads the logged in user
+ function loadUserInfo(user){
+//Finds the user
+  for (var i = 0; i < users.length; i++) {
+    if(users[i].username === user){
+      //This variable allows for the user's id to be called anywhere
+      userID = i; 
+      //Loads user's friend list
+      for (var x = 0; x < users[i].friends.length; x++) {
+        addProfile("friendsList", users[users[i].friends[x]].username, users[users[i].friends[x]].pfp);
+      }
+
+      // Updates user stat bar
+      const numStudyGroups = document.getElementById("studyStats");
+      const numFriends = document.getElementById("friendStats");
+      userPFP.setAttribute("src", users[i].pfp);
+      numStudyGroups.innerHTML = users[i].studyGroups.length;
+      numFriends.innerHTML = users[i].friends.length;
+     }
+  }
+
+ }
